31e550c76059974e35d8bbda5b030e0b3a790d7c
hyeoncheolkim <hyeoncheolkim96@gmail.com>
2020-07-17 22:04:33 +0900

    signup, login, makeRoom

---
+++ index.js
@@ -0,0 +1,22 @@
+const { GraphQLServer } = require('graphql-yoga');
+const resolvers = require('./graphql/resolvers');
+const { prisma } = require('./prisma/generated/prisma-client');
+
+const server = new GraphQLServer({
+    typeDefs: 'graphql/schema.graphql',
+    resolvers,
+    context: (request) => {
+        return { ...request, prisma };
+    },
+});
+server.start(
+    {
+        port: process.env.PORT || 5000,
+        endpoint: '/graphql',
+        playground: '/playground',
+        subscriptions: '/subscriptions',
+    },
+    ({ port }) => {
+        console.log('graphql server started', port);
+    }
+);
